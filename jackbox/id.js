d047a8b71469c6c783c235679e3858a285ce9068
finn021 <finn.burger17@gmail.com>
2025-03-24 18:56:57 +1100

    Update id.js

--- jackbox/id.js
+++ jackbox/id.js
@@ -3,8 +3,8 @@ const binUrl = 'https://api.jsonbin.io/v3/b/67e10e718a456b79667b8270/latest';
 fetch(binUrl)
   .then(response => response.json())
   .then(data => {
-    const videoID = data.video_id;
-    console.log(videoId);
+    const videoID = data.record.video_id;
+    console.log(videoID);
   })
   .catch(error => {
     console.error('Error fetching the video ID:', error);
