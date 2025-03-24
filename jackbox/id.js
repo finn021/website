9f1d2fda58468d011a1005aeb090c028faa24ee4
finn021 <finn.burger17@gmail.com>
2025-03-24 18:53:08 +1100

    Update id.js

--- jackbox/id.js
+++ jackbox/id.js
@@ -1,1 +1,11 @@
-const videoID = "PXpZiJ0EMvw"
+const binUrl = 'https://api.jsonbin.io/v3/b/67e10e718a456b79667b8270/latest';
+
+fetch(binUrl)
+  .then(response => response.json())
+  .then(data => {
+    const videoID = data.video_id;
+    console.log(videoId);
+  })
+  .catch(error => {
+    console.error('Error fetching the video ID:', error);
+  });
